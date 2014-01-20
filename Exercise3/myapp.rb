19b9ff4eea698b3e7116c813a2a07d168bec8fc5
Arvind <arvindvyas07@gmail.com>
2014-01-20 09:09:20 +0530

    add the methods

--- Exercise3/myapp.rb
+++ Exercise3/myapp.rb
@@ -1,5 +1,8 @@
 class MyApp
+  def initialize
+    @show = lambda{|env| env.keys.map {|key| "<b>key</b> => #{env[key]} <br/>"}}
+  end
   def call env
-  [200,{'Content_type'=>'text/plain'},["#{env}"]]
+    [200,{'Content_type'=>'text/plain'},@show.call(env)]
   end
 end	
